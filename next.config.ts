c910517c843da098c9b7bdeb16eb6187830d75c3
YourNewUsername <anandkulkarni7795651267@gmail.com>
2026-08-25 23:32:34 +0530

    chore: update next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: [
+    "riley-arturo-unjudicable.ngrok-free.dev",
+    "*.ngrok-free.dev",
+    "*.ngrok-free.app",
+    "*.ngrok.io",
+    "*.devtunnels.ms",
+    "10.182.211.233",
+    "10.182.211.233:3000",
+    "localhost:3000",
+    "127.0.0.1:3000",
+  ],
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8081/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
